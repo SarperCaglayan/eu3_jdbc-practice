d2bb7bb8f3495b38c9bba6f99b6df126350acddd
ugurargun <sarpercaglayan16@gmail.com>
2021-08-26 00:39:13 +0300

    Awesome Petstore Test with path() method. Run at least twice, then it passes:)

--- src/test/java/CybertekApi/PetstoreTestsWithPathParameters.java
+++ src/test/java/CybertekApi/PetstoreTestsWithPathParameters.java
@@ -13,23 +13,43 @@ public class PetstoreTestsWithPathParameters {
 
         RestAssured.baseURI= "https://petstore.swagger.io/v2";
     }
-
+        /*  Given accept type is Json
+            And path param id is 100
+            When user send a get request to endpoint
+            Then status code is 200
+            And Content type is "application/json
+            And response payload values match with the followings:
+                        id is 100,
+                        name is doggie,
+                        photoUrls is [String]
+                        status is available
+         */
 
     @Test
     public void pathQueryTest1(){
 
         Response response = RestAssured.given().accept(ContentType.JSON)
-                .and().pathParam("id", "9")
+                .and().pathParam("id", "100")
                // .and().pathParam("name","Bela")
                 .when().get("/pet/{id}");
 
         assertEquals(response.statusCode(),200);
         assertEquals(response.contentType(),"application/json");
-        assertTrue(response.body().asString().contains("King Kong"));
+        assertTrue(response.body().asString().contains("Carpi"));
 
         System.out.println("response.body().prettyPrint() = " + response.body().prettyPrint());
 
+        System.out.println("Id = " + response.body().path("id").toString());
+        System.out.println("photoUrls =" + response.body().path("photoUrls").toString());
+        System.out.println("status = " + response.body().path("status"));
 
+        int id= response.body().path( "id");
+        String name = response.body().path( "name");
+        String status = response.body().path("status");
+
+        assertEquals(id,100);
+        assertEquals(name,"doggie");
+        assertEquals(status,"available");
     }
 
     @Test
@@ -45,7 +65,7 @@ public class PetstoreTestsWithPathParameters {
 
         System.out.println( "response.body().prettyPrint() = " + response.body().prettyPrint());
 
-      //  System.out.println("Id:" +"response.body().path(\"id\") = " + response.body().path("id").toString());
+
     }
 
 }
