b5667f221445cd47a336f79e786c609f9b76df7b
ugurargun <sarpercaglayan16@gmail.com>
2021-09-02 00:35:02 +0300

    Awesome Petstore Test with MatchersForsolds method. Run at least twice, then it passes:)
    values are not static

--- src/test/java/PetstoreApi/HamcrestTest.java
+++ src/test/java/PetstoreApi/HamcrestTest.java
@@ -12,7 +12,7 @@ import static io.restassured.RestAssured.*;
     @Test
 public class HamcrestTest {
 
-        public void MatchersforSold() {
+        public void MatchersforPet() {
             given().accept(ContentType.JSON)
                     .and().pathParam("id", 7574746)
                     .when().log().all().get("https://petstore.swagger.io/v2/pet/{id}")
@@ -24,5 +24,16 @@ public class HamcrestTest {
                             "name", equalTo("Hardy"),
                             "status", equalTo("sold"));
 
+        }
+
+        public void MatchersForSoLds(){
+
+            given().when().get("https://petstore.swagger.io/v2/pet/findByStatus?status=sold")
+                    .then().assertThat().statusCode(200)
+                    .assertThat().contentType(equalTo("application/json"))
+                    .and().assertThat().header("Access-Control-Allow-Methods",equalTo("GET, POST, DELETE, PUT"))
+                    .and().header("Connection",equalTo("keep-alive"))
+                    .and().assertThat().body("id[0]",equalTo(9223372000666081598L),
+                        "category[0].name", equalTo("culpa cillum fugiat"));
         }
     }
